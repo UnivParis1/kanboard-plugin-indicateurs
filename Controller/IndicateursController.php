d121c90e94fba447ffbfb9caddb177281f47a189
Pascal Rigaux <Pascal.Rigaux@univ-paris1.fr>
2018-09-24 21:05:13 +0200

    allow "Adjoint au chef de service" to reflect the service
    https://projets.univ-paris1.fr/project/14/task/291

--- Controller/IndicateursController.php
+++ Controller/IndicateursController.php
@@ -5,7 +5,7 @@ use Kanboard\Controller\BaseController;
 class IndicateursController extends BaseController
 {
     static $roleForService = 'Directeur de projet';
-    static $group2service_regexp = '/Chef de service (.*)/';
+    static $group2service_regexp = '/^(?:Chef de service|Adjoint au chef de service) (.*)/';
 
     static $tasksProgress = [
         'En attente' => 0,
